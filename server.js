2e483dfdae74001f1af23c793a76c266b06c3bca
Umer-Aziz <umars25271997@gmail.com>
2021-01-07 22:06:25 +0500

    Adding Cart page ,login and Register Page

--- server.js
+++ server.js
@@ -4,18 +4,33 @@ const ejs=require('ejs');
 const path=require('path');
 const expressLayouts=require("express-ejs-layouts");
 
-//seting port.
-const PORT= process.env.PORT || 3000;
-// setting static path
 
-app.use(express.static('public'))
-app.get("/",(req,res)=>{
-    res.render("home")
-})
+const PORT= process.env.PORT || 3000;
+
 // setting ejs template engine
 app.use(expressLayouts);
 app.set("views",path.join(__dirname,"/resources/views"));
 app.set("view engine","ejs");
+
+// setting static path
+app.use(express.static('public'));
+
+app.get("/",(req,res)=>{
+    res.render("home")
+});
+
+app.get("/cart",(req,res)=>{
+    res.render("customers/cart")
+});
+
+app.get("/login",(req,res)=>{
+    res.render("auth/login")
+});
+
+app.get("/register",(req,res)=>{
+    res.render("auth/register")
+});
+
 
 app.listen(PORT,()=>{
     console.log(`The Server run at Port:${PORT}`);
